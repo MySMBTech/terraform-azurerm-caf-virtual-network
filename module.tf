30b9c32c314b03556f5b46ea29dc43259b4f3042
Nicolas Yuen <nicolas.yuen@gmail.com>
2020-06-05 17:54:50 +0800

    adding naming convention with prefix,postfix and subnet map

--- module.tf
+++ module.tf
@@ -1,17 +1,11 @@
 // Creates the networks virtual network, the subnets and associated NSG, with a special section for AzureFirewallSubnet
-# module "caf_name_vnet" {
-#   source  = "aztfmod/caf-naming/azurerm"
-#   version = "~> 0.1.0"
-  
-#   name    = var.networking_object.vnet.name
-#   type    = "vnet"
-#   convention  = var.convention
-# }
-
 resource "azurecaf_naming_convention" "caf_name_vnet" {  
-  name             =  var.networking_object.vnet.name
-  resource_type    = "vnet"
-  convention       = var.convention
+  name          = var.networking_object.vnet.name
+  prefix        = var.prefix != "" ? var.prefix : null
+  postfix       = var.postfix != "" ? var.postfix : null
+  max_length    = var.max_length != "" ? var.max_length : null
+  resource_type = "azurerm_virtual_network"
+  convention    = var.convention
 }
 
 resource "azurerm_virtual_network" "vnet" {
